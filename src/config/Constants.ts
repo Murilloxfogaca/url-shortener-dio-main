5554ef6b134c3895ebe19f34ad4d6ab3d77d5ffc
murilloxFogaca <sxecesar@gmail.com>
2021-11-14 01:03:34 -0300

    refactor: remove constant

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://MurilloMongo:eduzzsenha@url-shortnames-dio.aivnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		'#',
 }
